7cf82cff71499e1a146daae506877f9db061e065
Ahmad Alhamoud <alhamoudahmad2000@gmail.com>
2024-08-21 14:48:15 +0300

    implements security configration

---
+++ src/main/java/com/ahmad/social/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.ahmad.social.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(
+                c -> c.disable()
+                )
+                .authorizeHttpRequests(
+                        a -> a.anyRequest().authenticated()
+                )
+                .oauth2Login(withDefaults());
+        return http.build();
+    }
+
+}
